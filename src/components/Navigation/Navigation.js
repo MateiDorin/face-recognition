7c5b2b20e47e7699a536bdd59d3b8d26dda2a4f2
Dorin Matei <matei.dorin22@gmail.com>
2022-08-08 10:45:46 +0100

    sign out component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import './Navigation.css'
+
+const Navigation = () => {
+    return (
+        <div>
+            <nav className="sign-out">Sign Out</nav>
+        </div>
+    );
+};
+
+export default Navigation;
