d5c4a11b2310a417d50d66fab03507452a68037f
Dorin Matei <matei.dorin22@gmail.com>
2022-08-08 14:10:03 +0100

    created rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import "./Rank.css";
+
+const Rank = () => {
+  return (
+    <div>
+      <div>
+        <p className="rank">Dorin, your current rank is ...</p>
+      </div>
+      <div>
+        <p className="rank">#5</p>
+      </div>
+    </div>
+  );
+};
+
+export default Rank;
